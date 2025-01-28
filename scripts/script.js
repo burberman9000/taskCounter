109f7b0f57b100151f511115e4f836e7a441904f
Ivan <burdolevich@gmail.com>
2025-01-28 17:19:32 +0300

    fix: add some functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 const number = document.querySelector('.number__holder');
-const button = document.querySelector('.button');
+const button = document.querySelector('.button.increaseScore');
+const themeSwitcher = document.querySelector('.buttonLightSwitcher');
+const resetButton = document.querySelector('.resetScore');
+const body = document.querySelector('body');
 let initNum = 0;
+let lightMode = false;
 
 number.innerHTML = initNum;
 
 const switcher = () => {
     initNum++;
+    localStorage.setItem('score', initNum);
     number.innerHTML = initNum;
     if (initNum < 10) {
         number.style.color = 'red';
@@ -21,3 +26,43 @@ const switcher = () => {
 };
 
 button.addEventListener('click', switcher);
+
+const themeApplier = () => {
+    lightMode = !lightMode; // Toggle lightMode
+    if (lightMode) {
+        body.style.backgroundColor = 'white';
+        localStorage.setItem('lightMode', true);
+    } else {
+        body.style.backgroundColor = '#0d1117';
+        localStorage.setItem('lightMode', false);
+    }
+};
+
+const initGame = () => {
+    let currentScore = localStorage.getItem('score');
+    if (currentScore) {
+        initNum = currentScore;
+        number.innerHTML = initNum;
+        // Update color based on the score
+        switcher();
+    }
+    let currentTheme = localStorage.getItem('lightMode');
+    lightMode = currentTheme === 'true';
+
+    if (lightMode) {
+        body.style.backgroundColor = 'white';
+    } else {
+        body.style.backgroundColor = '#0d1117';
+    }
+};
+
+const scoreReseter = () => {
+    localStorage.removeItem('score'); // Correct way to remove score
+    number.style.color = 'white';
+    initNum = 0;
+    number.innerHTML = initNum;
+};
+
+initGame();
+themeSwitcher.addEventListener('click', themeApplier);
+resetButton.addEventListener('click', scoreReseter);
